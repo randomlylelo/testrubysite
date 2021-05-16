f38d70ea76ccf1d672798ad9856550df51b221d6
Leo <38267386+randomlylelo@users.noreply.github.com>
2021-05-16 09:07:59 -0400

    setup routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def index
     @users = Person.all
   end
+
+  def show
+    @user = Person.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "users#index"
 
-  get "/users", to: "users#index"
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
